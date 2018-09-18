ab8b6adfcf73283b5eba9b5bde8d410e153e5e74
eryktrzeciakiewicz <eryk.trzeciakiewicz@gmail.com>
2018-09-18 17:49:00 +0200

    Example has been finished

---
+++ Request/RequestHost/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+using Request;
+
+namespace RequestHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var baseAddress = new Uri("http://localhost:8000/Request");
+            var selfHost = new ServiceHost(typeof(Request.Request), baseAddress);
+            try
+            {
+                selfHost.AddServiceEndpoint(typeof(IRequest), new WSHttpBinding(), "RequestService");
+                var smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                selfHost.Description.Behaviors.Add(smb);
+                selfHost.Open();
+                Console.WriteLine("The serbice is now open");
+                Console.WriteLine();
+                Console.WriteLine("Press <ENTER> to stop");
+                Console.WriteLine();
+                Console.ReadLine();
+                selfHost.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Request/RequestServiceClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RequestServiceClient.ServiceReference1;
+
+namespace RequestServiceClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RequestClient client = new RequestClient();
+            client.SendRequest("I work!");
+        }
+    }
+}

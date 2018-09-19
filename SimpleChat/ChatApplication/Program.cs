0210beec0d6690fd1734b2b926e956a563ee803b
eryktrzeciakiewicz <eryk.trzeciakiewicz@gmail.com>
2018-09-19 18:11:59 +0200

    Both client side and server side are now functional

---
+++ SimpleChat/ChatApplication/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose your username");
+            var username = Console.ReadLine();
+            var user = new User(username);
+            user.Join();
+            while (true)
+            {
+                var msg = Console.ReadLine();
+                user.Write(msg);
+            }
+        }
+    }
+}
---
+++ SimpleChat/ChatApplication/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChatApplication.ServiceReference1;
+
+namespace ChatApplication
+{
+    class User
+    {
+        public string Nickname { get; set; }
+        private ChatServiceClient _client;
+
+        public User(string nickname)
+        {
+            Nickname = nickname;
+            _client = new ChatServiceClient();
+        }
+
+        public void Join()
+        {
+            _client.ReceiveMessage($"{Nickname} has joined!");
+        }
+
+        public void Leave()
+        {
+            _client.ReceiveMessage($"{Nickname} has left.");
+        }
+
+        public void Write(string msg)
+        {
+            _client.ReceiveMessage(Nickname+": "+ msg);
+        }
+    }
+}

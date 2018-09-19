17fe4cd3510059de6d80684f1c82486e6b28f2a0
eryktrzeciakiewicz <eryk.trzeciakiewicz@gmail.com>
2018-09-19 17:58:33 +0200

    Remote interface defined and implemented

---
+++ SimpleChat/ChatHost/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleChat;
+
+namespace ChatHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var baseuri = new Uri("http://localhost:8000");
+            var host = new ServiceHost(typeof(ChatService), baseuri);
+            try
+            {
+                host.AddServiceEndpoint(typeof(IChatService), new BasicHttpBinding(), "Chat");
+                var smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                host.Description.Behaviors.Add(smb);
+                host.Open();
+                Console.WriteLine("Chat is ready!");
+                Console.ReadLine();
+                host.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                host.Abort();
+            }
+        }
+    }
+}
